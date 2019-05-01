51805fd57d5d8b656220350ac77fae15964eeb28
petrovic-ivan <ivan.petrovic.srb@outlook.com>
2019-04-30 20:26:48 -0700

    Added Selection sort.

--- Arrays/SelectionSort.java
+++ Arrays/SelectionSort.java
@@ -1,14 +1,18 @@
-// Swaps elements and moves to sorted partiotion.
-public class BubbleSort {
+// Selects larges value in unsorted partition and move into sorted partition.
+public class SelectionSort {
     public static void main(String[] args) {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
         for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
-            for (int i = 0; i < lastUnsortedIndex; i++) {
-                if (intArray[i] > intArray[i + 1]) {
-                    swap(intArray, i, i + 1);
+
+            int largestIndex = 0;
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[largestIndex]) {
+                    largestIndex = i;
                 }
             }
+
+            swap(intArray, largestIndex, lastUnsortedIndex);
         }
 
         for (int i = 0; i < intArray.length; i++) {
@@ -25,4 +29,5 @@ public class BubbleSort {
         array[i] = array[j];
         array[j] = temp;
     }
+
 }
