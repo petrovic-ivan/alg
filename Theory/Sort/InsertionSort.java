8144cb1bc060fa90b3d25b47810868730ddab2c3
petrovic-ivan <ivan.petrovic.srb@outlook.com>
2019-05-05 15:17:21 -0700

    Added some comments.

--- Theory/Sort/InsertionSort.java
+++ Theory/Sort/InsertionSort.java
@@ -1,20 +1,23 @@
-// Insertion sort iterates, consuming one input element each repetition, and growing a sorted partition. 
-// At each iteration, insertion sort removes one element from the unsorted partition, finds the location it belongs within the sorted partition, and inserts it there. 
-// It repeats until no input elements remain.
+// Insertion sort is in-place algorithm.
+// - It iterates over array and traverse back in each iteration. 
+// - In each iteration it stores value of current iteration index (which also represents first unsorted index).
+// - Then it traverse back from first iteration index towards beginning.
+// - During traversion it compares current iteration value with current traversion value. 
+// - If traversion value is greater than current iteration value then it insert iteration value one place further.
+// - It stops traversion once if traversion value is not greater than current iteration value.
+// - Once traversion is stopped it inserts current iteration value in array.
+
 public class InsertionSort {
     public static void main(String[] args) {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
         for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
-           
-            int newElement = intArray[firstUnsortedIndex];
+            int currentIterationValue = intArray[firstUnsortedIndex];
             int i;
-            // Traversing from right to left.
-            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
-                intArray[i] = intArray[i - 1] ;
+            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > currentIterationValue; i--) {
+                intArray[i] = intArray[i - 1];
             }
-
-            intArray[i] = newElement;
+            intArray[i] = currentIterationValue;
         }
 
         for (int i = 0; i < intArray.length; i++) {
