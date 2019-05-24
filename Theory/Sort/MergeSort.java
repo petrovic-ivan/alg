31dc56dcf2230ad630afbb0e6e6ef65f464dbb96
petrovic-ivan <ivan.petrovic.srb@outlook.com>
2019-05-23 17:49:57 -0700

    Added some debugging.

--- Theory/Sort/MergeSort.java
+++ Theory/Sort/MergeSort.java
@@ -3,24 +3,26 @@ public class MergeSort {
 
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
-        mergeSort(intArray, 0, 6);
+        mergeSort(intArray, 0, intArray.length, "rignt");
 
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.println(intArray[i]);
-        }
+        // for (int i = 0; i < intArray.length; i++) {
+        //     System.out.println(intArray[i]);
+        // }
 
     }
 
-    public static void mergeSort(int[] input, int start, int end) {
-
+    public static void mergeSort(int[] input, int start, int end, String o) {
+        System.out.println("Start: " + start + " End: " + end + " " + o);
         if (end - start < 2) {
+            System.out.println("return ---------------");
             return; 
         }
 
         int mid = (start + end) / 2;
-        mergeSort(input, start, mid);
-        mergeSort(input, mid, end);
-        merge(input, start, mid, end);
+        mergeSort(input, start, mid, "right");
+        mergeSort(input, mid, end, "left");
+        
+        // merge(input, start, mid, end);
     }
 
     public static void merge(int[] input, int start, int mid, int end) {
