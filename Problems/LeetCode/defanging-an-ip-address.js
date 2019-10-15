3243fb0ce8343f88adcaf99cac0b661547bfb2d5
petrovic-ivan <ivan.petrovic.srb@outlook.com>
2019-10-14 17:08:52 -0700

    Refactoring.

--- Problems/LeetCode/defanging-an-ip-address.js
+++ Problems/LeetCode/defanging-an-ip-address.js
@@ -17,8 +17,7 @@ let defangIPaddr = function(address) {
     return address.replace(/\./g, '[.]');
 };
 
-console.log('Example 1: Result - ' + defangIPaddr('1.1.1.1') + ' Is Equal: ' +  (defangIPaddr('1.1.1.1') === '1[.]1[.]1[.]1'));
-console.log('Example 2: Result - ' + defangIPaddr('255.100.50.0') + ' Is Equal: ' +  (defangIPaddr('1.1.1.1') === '255[.]100[.]50[.]0'));
+
 
 
 // Used manual approach with for loop.
@@ -33,6 +32,11 @@ let defangIPaddr2 = function(address) {
     }
     return newaddress;
 };
+
+
+console.log('Example 1: Result - ' + defangIPaddr('1.1.1.1') + ' Is Equal: ' +  (defangIPaddr('1.1.1.1') === '1[.]1[.]1[.]1'));
+console.log('Example 2: Result - ' + defangIPaddr('255.100.50.0') + ' Is Equal: ' +  (defangIPaddr('1.1.1.1') === '255[.]100[.]50[.]0'));
+console.log('\n');
 
 console.log('Example 1: Result - ' + defangIPaddr2('1.1.1.1') + ' Is Equal: ' +  (defangIPaddr2('1.1.1.1') === '1[.]1[.]1[.]1'));
 console.log('Example 2: Result - ' + defangIPaddr2('255.100.50.0') + ' Is Equal: ' +  (defangIPaddr2('255.100.50.0') === '255[.]100[.]50[.]0'));
