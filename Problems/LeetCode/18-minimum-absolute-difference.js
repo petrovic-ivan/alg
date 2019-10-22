bffd87c04912db75b6b65e2364855c73be9f3d5c
petrovic-ivan <ivan.petrovic.srb@outlook.com>
2019-10-22 13:53:58 -0700

    Added more solutions.

--- Problems/LeetCode/18-minimum-absolute-difference.js
+++ Problems/LeetCode/18-minimum-absolute-difference.js
@@ -29,23 +29,59 @@ Constraints:
 
 */
 
-let minimumAbsDifference = function(arr) {
-    const pairs = [];
-    let min = Number.MAX_SAFE_INTEGER;
-    arr = arr.sort((a, b) => {
-        let diff =  Math.abs(a - b);
-        min = min > diff ? diff : min;
-        return a - b; 
-    });
-    for (let i = 0; i < arr.length - 1; i++) {
-        if ((arr[i + 1] - arr[i]) === min) {
-            pairs.push([arr[i], arr[i + 1]]);
-        }
-        
+const minimumAbsDifference2 = arr => {
+    const RANGE = 10 ** 6;
+    const data = new Int8Array(RANGE * 2 + 1);
+    let min = max = arr[0];
+    for (let i = 0; i < arr.length; ++i) {
+        data[arr[i] + RANGE] = 1;
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
     }
-    return pairs;
+    let ret = [];
+    let diff = max - min;
+    let prev = min + RANGE;
+    for (let i = prev + 1; i <= max + RANGE; ++i) {
+        if (data[i] === 0) continue;
+        if (i - prev === diff) {
+            ret.push([prev - RANGE, i - RANGE]);
+        } else if (i - prev < diff) {
+            diff = i - prev;
+            ret = [[prev - RANGE, i - RANGE]];
+        }
+        prev = i;
+    }
+    return ret;
 };
 
-console.log('Example 1: Result - ' + minimumAbsDifference([4,2,1,3]) + ' Expected: [[1,2],[2,3],[3,4]]');
-console.log('Example 2: Result - ' + minimumAbsDifference([1,3,6,10,15]) + ' Expected: [[1,3]]');
-console.log('Example 3: Result - ' + minimumAbsDifference([3,8,-10,23,19,-4,-14,27]) + ' Expected: [[-14,-10],[19,23],[23,27]]');
+let minimumAbsDifference = function (arr) {
+    const RANGE = 10 ** 6;
+    let result = [];
+    const sorted = new Int8Array(2 * RANGE + 1);
+    let min = max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        sorted[RANGE + arr[i]]++;
+        if (min > arr[i]) min = arr[i];
+        if (max < arr[i]) max = arr[i];
+    }
+
+    let diff = max - min;
+    let prev = min + RANGE;
+
+    for (let i = prev + 1; i <= RANGE + max; i++) {
+        if (sorted[i] === 0) continue;
+        if (i - prev === diff) {
+            result.push([prev - RANGE, i  - RANGE]);
+        } else if ((i - prev) < diff) {
+            result = [[prev - RANGE, i - RANGE]];
+            diff = i - prev;
+        }
+        prev = i;
+    }
+
+    return result;
+}
+
+console.log('Example 1: Result - ' + minimumAbsDifference([4, 2, 1, 3]) + ' Expected: [[1,2],[2,3],[3,4]]');
+console.log('Example 2: Result - ' + minimumAbsDifference([1, 3, 6, 10, 15]) + ' Expected: [[1,3]]');
+console.log('Example 3: Result - ' + minimumAbsDifference([-14, -10, -4, 3, 8, 19, 23, 27]) + ' Expected: [[-14,-10],[19,23],[23,27]]');
